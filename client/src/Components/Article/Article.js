ef444473e98ae6c500456f364cbc133162f294fe
melissaprax <melissaprax@gmail.com>
2018-06-30 15:22:58 -0500

    tweaked app and homejs

---
+++ client/src/Components/Article/Article.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Article = ({title, url, _id, date, handleClick, buttonText, saved}) => (
+
+    <div>
+        <em>{title}</em>
+        <button onClick={() => handleClick(_id)}>
+            {buttonText}
+        </button>
+    </div>
+
+);
+
+export default Article; 
